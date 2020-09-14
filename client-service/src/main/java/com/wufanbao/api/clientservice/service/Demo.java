0082bfe720c21d16822c45569a6dc67d0b504b7f
张帅 <1058784921@qq.com>
2020-09-14 22:37:49 +0800

    c:demo.java 222

--- client-service/src/main/java/com/wufanbao/api/clientservice/service/Demo.java
+++ client-service/src/main/java/com/wufanbao/api/clientservice/service/Demo.java
@@ -10,7 +10,7 @@ public class Demo {
     public static void main(String []args) {
         //c:111
         //d:111
-
+        //C:222
 
         HashMap map=new HashMap<String,Object>();
         map.put("machineId", "001");
